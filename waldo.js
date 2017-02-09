1b1d97437d0623a17b9646438c5d8c2b5477eaf4
Zuzana Toldyova <zuzka@localhost.localdomain>
2017-02-09 11:35:30 -0800

    modified to use forEach loop

--- waldo.js
+++ waldo.js
@@ -1,10 +1,15 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(person, index){
+    if (person === "Waldo"){
+      found(index);
     }
-  }
+  })
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+  //     found(i);   // execute callback
+  //   }
+  // }
 }
 
 function actionWhenFound(x) {
